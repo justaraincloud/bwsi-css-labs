efaac97b7ba303058c57fa31ed4e70f1878ee8d1
justaraincloud <josepgluan2011@gmail.com>
2026-03-25 22:48:21 -0400

    A
    A

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -37,6 +37,15 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
 
+def request_sanitized_number(prompt:str) -> float:
+
+    while True:
+        try:
+            number = float(input(prompt))
+            return(number)
+        except ValueError:
+            print("Invalid Input. Please enter a valid number.")
+
 def main():
     
     print(f"===== Simple Calculator =====")
